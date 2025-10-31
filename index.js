d2625df720e8ae819e41484752cb8b9f054aa099
Daria Steblovska <darias1896.96@gmail.com>
2025-10-31 02:16:12 +0000

    routes added

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-console.log('hello')
-const os = require('os')
-let res = os.platform()
-console.log(res);
+// console.log('hello')
+// const os = require('os')
+// let res = os.platform()
+// console.log(res);
+
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    render('index')
+})
+app.get('/login', (req, res) => {
+    render('login')
+})
+app.get('/register', (req, res) => {
+    render('register')
+})
